67d5cbe3719c630d81533f0d19ef57557988635b
arthur <arthur.chu@automattic.com>
2021-09-27 18:56:31 +0800

    Add iframe args to getDesignUrl so that it can be used by WebPreview component

--- client/signup/steps/design-picker/index.jsx
+++ client/signup/steps/design-picker/index.jsx
@@ -131,7 +131,7 @@ class DesignPickerStep extends Component {
 		} = this.props;
 
 		const { selectedDesign } = this.state;
-		const previewUrl = getDesignUrl( selectedDesign, locale );
+		const previewUrl = getDesignUrl( selectedDesign, locale, { iframe: true } );
 
 		return (
 			<WebPreview
@@ -146,8 +146,6 @@ class DesignPickerStep extends Component {
 					components: { strong: <strong /> },
 				} ) }
 				toolbarComponent={ PreviewToolbar }
-				// Avoid page direct because checkForIframeLoadFailure inside the WebPreview returns true but I'm not sure why
-				disableRedirects
 			/>
 		);
 	}
